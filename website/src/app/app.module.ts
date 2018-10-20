79a463ea6ed1286b00cafda2394d4ff211207c97
unknown <behzad.b.i.g@gmail.com>
2018-10-20 12:37:52 +0330

    First commit

---
+++ website/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContentComponent } from './content/content.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { ClientsComponent } from './clients/clients.component';
+import { PriceingtableComponent } from './priceingtable/priceingtable.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SocialComponent } from './social/social.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IntroComponent,
+    GalleryComponent,
+    ContentComponent,
+    TestimonialComponent,
+    ClientsComponent,
+    PriceingtableComponent,
+    FooterComponent,
+    HeaderComponent,
+    SocialComponent,
+    NavigationComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
